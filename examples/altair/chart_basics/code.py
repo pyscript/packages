36de3cd6d7700c7c86d4b7d229a3d5e96fe4bd14
Nicholas H.Tollervey <ntollervey@anaconda.com>
2026-05-29 16:30:01 +0100

    Move setups to first example.

--- examples/altair/chart_basics/code.py
+++ examples/altair/chart_basics/code.py
@@ -8,7 +8,40 @@ Altair charts are built by combining three things:
 
 See https://altair-viz.github.io for the full documentation.
 """
+import altair as alt
+import pandas as pd
 from IPython.core.display import display, HTML
+
+
+
+def show_chart(chart):
+    """Render an Altair chart as inline HTML via Vega-Embed."""
+    spec = chart.to_json()
+    html = f"""
+    <div class="altair-chart"></div>
+    <script src="https://cdn.jsdelivr.net/npm/vega@5"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-lite@5"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-embed@6"></script>
+    <script type="application/json" class="vega-spec">{spec}</script>
+    <script>
+      (function() {{
+        var scripts = document.getElementsByClassName('vega-spec');
+        var el = scripts[scripts.length - 1];
+        var spec = JSON.parse(el.textContent);
+        var target = el.previousElementSibling;
+        function embed() {{
+          if (window.vegaEmbed) {{
+            vegaEmbed(target, spec, {{actions: false}});
+          }} else {{
+            setTimeout(embed, 100);
+          }}
+        }}
+        embed();
+      }})();
+    </script>
+    """
+    display(HTML(html), append=True)
+
 
 heading("A bakery's daily sales")
 note(
--- examples/altair/chart_basics/setup.py
+++ examples/altair/chart_basics/setup.py
@@ -35,36 +35,3 @@ def heading(text, level=2):
 
 def note(text):
     display(HTML(f"<p>{text}</p>"), append=True)
-
-
-import altair as alt
-import pandas as pd
-
-
-def show_chart(chart):
-    """Render an Altair chart as inline HTML via Vega-Embed."""
-    spec = chart.to_json()
-    html = f"""
-    <div class="altair-chart"></div>
-    <script src="https://cdn.jsdelivr.net/npm/vega@5"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vega-lite@5"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vega-embed@6"></script>
-    <script type="application/json" class="vega-spec">{spec}</script>
-    <script>
-      (function() {{
-        var scripts = document.getElementsByClassName('vega-spec');
-        var el = scripts[scripts.length - 1];
-        var spec = JSON.parse(el.textContent);
-        var target = el.previousElementSibling;
-        function embed() {{
-          if (window.vegaEmbed) {{
-            vegaEmbed(target, spec, {{actions: false}});
-          }} else {{
-            setTimeout(embed, 100);
-          }}
-        }}
-        embed();
-      }})();
-    </script>
-    """
-    display(HTML(html), append=True)
